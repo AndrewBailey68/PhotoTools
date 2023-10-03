cf2b162f8d4e0c167f5dc76f4a73e32e885d9aa3
AndrewBailey68 <Andrew@cornisteanu.com>
2023-10-03 23:44:46 +0100

    new file:   Count images in directories.py

--- Count images in directories.py
+++ Count images in directories.py
@@ -1,64 +1,159 @@
+# - Walk through directories
+# - identify images
+# - count images by type
+# -
+#
+#
+#
+#
+
 import os
+import csv
 import cv2
-import rawpy
-import exifread
 import shutil
+import exifread
 from datetime import datetime
 import tkinter as tk
 from tkinter import Listbox, ttk
 from tkinter import filedialog
-
+from tqdm import tqdm
 
 # Source directory where photos are stored
 source_dir = '/Volumes/media1/Pictures/Photos/'
-display_time_ms = 1
-directory_paths = [source_dir]
-
 if not os.path.exists(source_dir):
     print("Path not found")
 
 # Destination directory where organized photos will be stored
 destination_dir = '/path/to/destination/directory'
 
+display_time_ms = 1
+directory_paths = [source_dir]
+image_number = 0
+image_data_cache = []
+
 image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.cr2', '.raw']
 cv2_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 raw_extensions = ['.cr2', '.raw']
+# ===============================================
+# Preprocess the image extensions to lowercase and convert them into a set for faster membership checking
+image_extensions_set = {ext.lower() for ext in image_extensions}
+cv2_extensions_set = {ext.lower() for ext in cv2_extensions}
+raw_extension_set = {ext.lower() for ext in raw_extensions}
+
+# Function to add image data to the cache
+
+
+def cache_image_data(filename, path, image_size, image_type):
+    image_data_cache.append({'filename': filename, 'path': path,
+                            'image_size': image_size, 'image_type': image_type})
+# ===============================================
+
+# Function to save cached image data to a CSV file
+
+
+def save_cached_data_to_csv():
+    with open('image_data.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Filename', 'Path', 'Image Size', 'Image Type'])
+        for item in image_data_cache:
+            csv_writer.writerow(
+                [item['filename'], item['path'], item['image_size'], item['image_type']])
+# ===============================================
+
+# Function to load image data from the CSV file into the cache
+
+
+def load_data_to_cache():
+    try:
+        with open('image_data.csv', 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                cache_image_data(row['Filename'], row['Path'],
+                                 row['Image Size'], row['Image Type'])
+    except FileNotFoundError:
+        pass  # Handle the case where the CSV file doesn't exist
+# ===============================================
+
+# Function to count directories recursively
+
+
+def count_directories(root_directory):
+    count = 0
+    for _, dirs, _ in os.walk(root_directory):
+        count += len(dirs)
+    return count
 
 
 def is_image_file(file_path):
-    return any(file_path.lower().endswith(ext) for ext in image_extensions)
+    # Extract the file extension and convert it to lowercase
+    file_extension = os.path.splitext(file_path)[1].lower()
+    # Check if the lowercase file extension is in the set of image extensions
+    return file_extension in image_extensions_set
 # ===============================================
+
+
+def count_image_files(directory):
+    image_count = 0
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if is_image_file(file_path):
+            image_count += 1
+            image_size = os.path.getsize(file_path)
+            cache_image_data(filename, directory, image_size,
+                             os.path.splitext(filename)[1].lower())
+    return image_count
+# ===============================================
+
+
+def isValInLst(val, lst):
+    return bool([index for index, content in enumerate(lst) if val in content])
+
 
 def find_image_directories(root_directory):
     image_directories = []
+    total_dirs = count_directories(root_directory)
+    pbar = tqdm(total=total_dirs, desc="Scanning Directories", unit="dir")
+
     for root, dirs, _ in os.walk(root_directory):
         for dir_name in dirs:
-            print(dir_name)
-            dir_path = os.path.join(root, dir_name)
-            # Check if the directory contains at least one image file
-            if any(is_image_file(os.path.join(dir_path, filename)) for filename in os.listdir(dir_path)):
-                image_directories.append(dir_path)
+            try:
+                # print(dir_name)
+                dir_path = os.path.join(root, dir_name)
+                # Check if the directory contains at least one image file
+                image_count = count_image_files(dir_path)
+                if image_count > 0:
+                    image_directories.append((dir_path, image_count))
+            except:
+                print("Caught an error")
+    # Time to cache the data
+            pbar.update(1)
+
     return image_directories
 # ===============================================
+
 
 def update_directory_list():
     root_source_dir = filedialog.askdirectory(title="Select Root Directory")
     if root_source_dir:
-        image_directories = find_image_directories(root_source_dir)
+        image_directories_tuple = []
+        image_directories_tuple = find_image_directories(root_source_dir)
         # Global variable to store all the directories we have images in
-
         listbox.delete(0, tk.END)  # Clear the listbox
-        for directory in image_directories:
-            listbox.insert(tk.END, directory)
+        for directory_tuple in image_directories_tuple:
+            directory, image_count = directory_tuple
+            listbox.insert(tk.END, f"{directory} ({image_count} images")
             directory_paths.append(directory)
 # ===============================================
 
 # Function to resize the listbox if the window size changes
+
+
 def resize_listbox(event):
     # Adjust height and width as needed
     listbox.config(height=(event.height // 20), width=(event.width // 20))
 # ===============================================
 # Function to display images from the selected directory
+
 
 def display_selected_directory():
     # index needs adjusting up by the base of the list isn't the same as the array
@@ -75,6 +170,8 @@ def display_selected_directory():
 # ===============================================
 
 # Function to read Exif data from an image file
+
+
 def read_exif_data(image_path):
     exif_data = ""
     with open(image_path, 'rb') as image_file:
@@ -86,6 +183,8 @@ def read_exif_data(image_path):
 # ===============================================
 
 # Function to display images and Exif data
+
+
 def display_image_with_exif(image_path):
     # Create a Tkinter window
     exif_window = tk.Toplevel()
@@ -102,17 +201,18 @@ def display_image_with_exif(image_path):
     exif_text.pack(fill=tk.BOTH, expand=True)
 # ===============================================
 
-
 # Function to walk through the directories and display the contents
+
+
 def display_images(image_directories):
-    #cv2.namedWindow("Slideshow", cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("Slideshow", cv2.WINDOW_NORMAL)
     # cv2.setWindowProperty( "Slideshow",cv2.WINDOW_AUTOSIZE)
     for filename in os.listdir(image_directories):
         file_path = os.path.join(image_directories, filename)
         if any(file_path.lower().endswith(ext) for ext in cv2_extensions):
             if is_image_file(file_path):
-                display_image_with_exif (file_path)
-                
+                display_image_with_exif(file_path)
+
                 # img = cv2.imread(file_path)
                 # if img is not None:
                 #     cv2.imshow("Slideshow", img)
@@ -155,10 +255,7 @@ image_button.pack()
 listbox = tk.Listbox(window)
 listbox.pack(fill=tk.BOTH, expand=True)  # Allow listbox to expand
 
-
 window.bind("<Configure>", resize_listbox)
-
 window.mainloop()
-
 
 print("Task completed.")
